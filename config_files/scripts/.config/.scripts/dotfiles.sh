b5a5961cd943e90295d982b02a26754031e6f380
Pratham Saraf <prathammsaraf@gmail.com>
2022-12-09 01:07:55 +0530

    removed unused script

--- config_files/scripts/.config/.scripts/dotfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Enter the commit message: "
-read $message
-cd $HOME/windows/drive_1/dotfiles/
-git add .
-git commit -m "$message" 
-git push -u origin main
-dunstify "updated dotfiles"
